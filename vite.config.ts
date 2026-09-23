92a3aa8e5b78d1af7d3709236805544940f2c364
imranhashmi24 <imranhashmi2742@gmail.com>
2026-09-23 11:23:47 +0600

    fix github pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,8 @@
-// import tailwindcss from '@tailwindcss/vite';
-// import react from '@vitejs/plugin-react'
-// import { defineConfig } from 'vite'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-// });
-
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/imranhashmi24-assignment-five/",
+  base: "/assignment-five/",
 });
